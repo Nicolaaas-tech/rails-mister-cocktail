01936d88dc5c4021c2a3f7a1714d7b0d5d7cbb40
Nicolas le Bechec de Redon <nicolaconilebechec@gmail.com>
2020-11-13 15:43:28 +0100

    nkzrjbvirz

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,4 @@
 class DosesController < ApplicationController
-  # before_action :set_dose, only: [:destroy]
-
-  def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
-
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -26,10 +19,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
-
-  # def set_dose
-  #   @dose = Dose.find(params[:id])
-  # end
 end
